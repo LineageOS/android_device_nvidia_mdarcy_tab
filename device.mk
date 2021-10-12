a780b3efba511b5a6c29ca7d57171892c8028500
Aaron Kling <webgeek1234@gmail.com>
2021-10-12 14:35:06 -0500

    Initial commit, import and rebrand from foster_tab

---
+++ device.mk
@@ -0,0 +1,14 @@
+$(call inherit-product, device/nvidia/mdarcy/device.mk)
+
+PRODUCT_CHARACTERISTICS := tablet
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    device/nvidia/foster_tab/overlay
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.software.device_admin.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_admin.xml \
+    frameworks/native/data/etc/android.software.managed_users.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.managed_users.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
